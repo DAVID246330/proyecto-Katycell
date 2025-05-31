665695c254765e297c1bbd5b8e15a3e2eebfb463
DAVID246330 <daviidpacheco2402@gmail.com>
2025-05-31 16:17:56 -0500

    cambios al formulario login

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -43,7 +43,7 @@ exports.login = (req, res) => {
 // REGISTRO
 exports.register = async (req, res) => {
     const { user, email, password, telefono, direccion } = req.body;
-    const rol = 'Administrador';
+    const rol = 'Cliente';
 
     if (!user || !email || !password || !telefono || !direccion) {
         return res.render('register', { mensajeError: 'Todos los campos son obligatorios.' });
